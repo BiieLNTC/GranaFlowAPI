bb3f78c01a1672d517b40b2509471cf2bd1d791f
Gabriel Squissato <biel.squissato@hotmail.com>
2026-03-20 22:55:10 -0300

    implementado endpoint  de teste da api

---
+++ GranaFlow.API/Controllers/HealthController.cs
@@ -0,0 +1,22 @@
+﻿using GranaFlow.Application.Auth;
+using GranaFlow.Application.Dtos;
+using GranaFlow.Application.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GranaFlow.API.Controllers
+{
+    public class HealthController : BaseController
+    {
+        public HealthController(IHttpContextAccessor httpContextAccessor, InfoToken infoToken) : base(httpContextAccessor, infoToken)
+        {
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public async Task<IActionResult> Health()
+        {
+            return Ok(DateTime.Now);
+        }
+    }
+}
